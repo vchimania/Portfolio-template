a5151b304025a170e61cbbb68302f14d6a8289ba
Vini Chimania <vinichimania98@gmail.com>
2023-08-21 00:14:05 +0530

    project link changed

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,17 +1,10 @@
 import {
-  mobile,
-  backend,
-  creator,
-  web,
   javascript,
   html,
   css,
   reactjs,
-  redux,
   tailwind,
-  nodejs,
   nextjs,
-  tesla,
   osm,
   crypto,
   today,
@@ -32,25 +25,6 @@ export const navLinks = [
   {
     id: "contact",
     title: "Contact",
-  },
-];
-
-const services = [
-  {
-    title: "Web Developer",
-    icon: web,
-  },
-  {
-    title: "React Native Developer",
-    icon: mobile,
-  },
-  {
-    title: "Backend Developer",
-    icon: backend,
-  },
-  {
-    title: "Content Creator",
-    icon: creator,
   },
 ];
 
@@ -141,33 +115,6 @@ const experiences = [
   },
 ];
 
-const testimonials = [
-  {
-    testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
-  },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
-];
-
 const projects = [
   {
     name: "OSM Maps",
@@ -188,7 +135,7 @@ const projects = [
       },
     ],
     image: osm,
-    source_code_link: "https://github.com/shazzy152/gran",
+    source_code_link: "https://github.com/vchimania/Maps-Search",
   },
   {
     name: "Crypto Tracker",
@@ -234,4 +181,4 @@ const projects = [
   },
 ];
 
-export { services, technologies, experiences, testimonials, projects };
+export { technologies, experiences, projects };

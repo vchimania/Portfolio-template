32d901e58dd31b3eb7c7b620494e873b27c4ac27
Vini Chimania <vinichimania98@gmail.com>
2023-08-21 12:01:34 +0530

    bug fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { technologies } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 import { SectionWrapper } from "../hoc";
+import { Tilt } from "react-tilt";
 
 const TechCard = ({ index, name, icon }) => {
   return (
